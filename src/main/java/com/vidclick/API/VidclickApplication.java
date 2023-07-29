e91b8470c033fe904b6fcf48a4cb67a07a4ec8bb
artem <artem@gmail.com>
2023-07-29 16:07:50 +0300

    core structure of the project

---
+++ src/main/java/com/vidclick/API/VidclickApplication.java
@@ -0,0 +1,13 @@
+package com.vidclick.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidclickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidclickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidclick/API/VidclickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidclick.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidclickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
