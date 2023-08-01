7038677996f69b67a1a1138a15420c1d0b898794
artem <artem@gmail.com>
2023-08-01 10:07:00 +0300

    test config

--- src/test/java/com/vidclick/API/VidclickApplicationTests.java
+++ src/test/java/com/vidclick/API/VidclickApplicationTests.java
@@ -1,11 +1,17 @@
 package com.vidclick.API;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode =  DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class VidclickApplicationTests {
 
+    @Autowired
+    TestRestTemplate testRestTemplate;
 	@Test
 	void contextLoads() {
 	}
