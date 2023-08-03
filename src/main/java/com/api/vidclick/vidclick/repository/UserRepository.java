87ce041af3ee7402407a3a56e692bd1bf6e6d9b8
Artem Fedorchenko <96698663+Artemmmm13@users.noreply.github.com>
2023-08-03 15:45:09 +0300

    Delete src directory

--- src/main/java/com/api/vidclick/vidclick/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.vidclick.vidclick.repository;
-
-import com.api.vidclick.vidclick.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-}
