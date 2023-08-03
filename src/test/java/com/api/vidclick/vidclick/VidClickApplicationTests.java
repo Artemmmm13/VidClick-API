9de2417f3d5dd7c17fe83f472a1eed02236dc083
Artem Fedorchenko <96698663+Artemmmm13@users.noreply.github.com>
2023-08-03 14:51:02 +0300

    Add files via upload

---
+++ src/test/java/com/api/vidclick/vidclick/VidClickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.vidclick.vidclick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidClickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
