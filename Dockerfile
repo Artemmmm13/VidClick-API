7bcffa912e83b0c3d67359537963881cd65b949c
artem <artem@gmail.com>
2023-08-06 13:23:16 +0300

    add get endpoint with id path variable

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM openjdk:20
-LABEL authors="godzilla"
-ADD target/VidClick.jar app.jar
-
-ENV MONGO_HOST vidclick-mongo
-ENV MONGO_PORT 27017
-
-ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/api/vidclick/packages/controllers/FundraisingController.java
+++ src/main/java/com/api/vidclick/packages/controllers/FundraisingController.java
@@ -2,9 +2,10 @@ package com.api.vidclick.packages.controllers;
 
 import com.api.vidclick.packages.models.FundraisingOffer;
 import com.api.vidclick.packages.repository.FundraisingOfferRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/fundraising")
@@ -23,5 +24,18 @@ public class FundraisingController {
     @GetMapping
     public List<FundraisingOffer> getFundraisingOffers(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<FundraisingOffer> findById(@PathVariable Integer requestedId) {
+        Optional<FundraisingOffer> optionalFundraisingOffer = repository.findById(requestedId);
+
+        if (optionalFundraisingOffer.isPresent()){
+            FundraisingOffer entity = optionalFundraisingOffer.get();
+            return ResponseEntity.ok(entity);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
