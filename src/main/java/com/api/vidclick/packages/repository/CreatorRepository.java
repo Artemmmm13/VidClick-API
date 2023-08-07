618baf36238ffd3ea20f81dbe83b2cc8a056d0cc
Artem Fedorchenko <96698663+Artemmmm13@users.noreply.github.com>
2023-08-07 16:31:23 +0300

    Delete src directory

--- src/main/java/com/api/vidclick/packages/repository/CreatorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.vidclick.packages.repository;
-
-import com.api.vidclick.packages.models.Creator;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CreatorRepository extends MongoRepository<Creator, Integer> {
-}
