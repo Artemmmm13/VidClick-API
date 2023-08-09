45378712668420c1b9f717791de97b17f3c09eb6
artem <artem@gmail.com>
2023-08-09 10:27:06 +0300

    add auth logic

--- server/src/main/java/com/api/vidclick/securityConfig/ApplicationConfig.java
+++ server/src/main/java/com/api/vidclick/securityConfig/ApplicationConfig.java
@@ -1,14 +1,16 @@
 package com.api.vidclick.securityConfig;
 
-
-import com.api.vidclick.models.Creator;
 import com.api.vidclick.repositories.CreatorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,5 +20,18 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService(){
         return username -> repository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User with such user name does not exits."));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
