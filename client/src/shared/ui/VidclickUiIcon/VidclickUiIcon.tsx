614812d9d50cb7cba6129c269858d7c02fd894bc
Ihor Bobulych <ihbobuacc@gmail.com>
2023-08-22 20:27:35 +0300

    Create own icon component

--- client/src/shared/ui/VidclickUiIcon/VidclickUiIcon.tsx
+++ client/src/shared/ui/VidclickUiIcon/VidclickUiIcon.tsx
@@ -1,7 +1,20 @@
-export function VidclickUiIcon() {
-    return (
-        <div>
+import {Box, SxProps} from "@mui/material";
+import {Link} from "react-router-dom"
+import logo from "./assets/logo.svg"
 
-        </div>
+interface IVidclickUiIconProps{
+    sx?: SxProps
+}
+
+export function VidclickUiIcon({sx}: IVidclickUiIconProps) {
+    return (
+        <Link to="/">
+            <Box
+                component="img"
+                sx={{height: 40, ...sx}}
+                alt="vidclick-ui-logo-svg"
+                src={logo}
+            />
+        </Link>
     );
 }
