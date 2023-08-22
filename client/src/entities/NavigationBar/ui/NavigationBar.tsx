ccf57939f6c07daa10a677884ebd798e981e9cde
Ihor Bobulych <ihbobuacc@gmail.com>
2023-08-22 20:44:08 +0300

    Add icon to navigation bar. TODO: Fix fonts, implement design

--- client/src/entities/NavigationBar/ui/NavigationBar.tsx
+++ client/src/entities/NavigationBar/ui/NavigationBar.tsx
@@ -1,7 +1,23 @@
+import {AppBar, Container, Toolbar, Typography} from "@mui/material";
+import {VidclickUiIcon} from "../../../shared/ui/VidclickUiIcon";
+
 export function NavigationBar() {
+    // const menuItems = [
+    //     {key: 'fundraisingOffers', label: "Fundraising Offers", link: "/offers"},
+    //     {key: 'about', label: "About Us", link: "/about"},
+    //     {key: 'signup', label: "Sign Up", link: "/signup"},
+    //     {key: 'login', label: "Log In", link: "/login"}
+    // ]
     return (
-        <div>
-            Navbar
-        </div>
+        <AppBar position="static"
+                color="inherit"
+                style={{fontFamily: 'Bricolage Regular'}}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <VidclickUiIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 10}}/>
+                    <Typography sx={{display: {xs: 'none', md: 'flex'}}}/>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
