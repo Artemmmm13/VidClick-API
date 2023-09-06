92291c5f64d80cbaf03ba5b613dc6a78e5f8321a
artem <artem@gmail.com>
2023-09-06 17:06:59 +0300

    improve logout operation

--- server/src/main/java/com/api/vidclick/services/LogoutService.java
+++ server/src/main/java/com/api/vidclick/services/LogoutService.java
@@ -1,14 +1,11 @@
 package com.api.vidclick.services;
 
 import com.api.vidclick.DTO.LogoutResponse;
-import com.api.vidclick.models.Creator;
-import com.api.vidclick.repositories.CreatorRepository;
 import com.api.vidclick.repositories.TokenRepository;
 import com.api.vidclick.token.Token;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.data.cassandra.CassandraRepositoriesAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +17,6 @@ import java.util.NoSuchElementException;
 @RequiredArgsConstructor
 public class LogoutService {
     private final TokenRepository repository;
-
 
     public ResponseEntity<LogoutResponse> logout(HttpServletRequest request){
         Cookie[] userCookies = request.getCookies();
@@ -36,13 +32,7 @@ public class LogoutService {
 
         List<Token> allCreatorTokens = repository.findAllValidTokenByCreator(creatorId);
 
-        for (Token token: allCreatorTokens){
-            token.setExpired(true);
-            token.setRevoked(true);
-            token.setToken("");
-            repository.save(token);
-        }
-
+        repository.deleteAll(allCreatorTokens);
         return ResponseEntity.status(200).body(new LogoutResponse(true));
     }
 }
